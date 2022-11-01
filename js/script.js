a6c6999f2c4b24df64eec64849c4bd8f4a51b193
bodwood <bodiewood@hotmail.com>
2022-11-01 16:07:11 -0700

    add balance functionality for deposits and withdrawals
    Co-authored-by: Lucas Mollerstuen <mollerstuen.works@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Holds instances of constructor
 function Bank() {
     this.accounts = {};
     this.currentId = 0;
@@ -5,7 +6,7 @@ function Bank() {
 
 Bank.prototype.addAccount = function (account) {
     account.id = this.assignId();
-    this.accounts[account.id] = account; 
+    this.accounts[account.id] = account;
 };
 
 Bank.prototype.assignId = function () {
@@ -13,28 +14,61 @@ Bank.prototype.assignId = function () {
     return this.currentId;
 };
 
-Bank.prototype.findAccount = function(id) {
+Bank.prototype.findAccount = function (id) {
     if (this.accounts[id] !== undefined) {
         return this.accounts[id];
     }
     return false;
 };
 
-function Account(firstName, initialDeposit, currentBalance) {
+//Constructor
+function Account(firstName, initialDeposit) {
     this.firstName = firstName;
     this.initialDeposit = initialDeposit;
-    this.currentBalance = currentBalance;
 }
 
 let bankAccount = new Bank();
 
-let person1 = new Account("Bodie", "12324");
-let person2 = new Account("Lucas", "23589412358");
-bankAccount.addAccount(person1);
-bankAccount.addAccount(person2);
-console.log(bankAccount.findAccount('2'));
+console.log(bankAccount)
+
 
 
 
 
 //------------------------------------------------------//
+
+function handleBalance(){
+    const depositAmount = parseInt(document.getElementById("depositAmount").value);
+    const withdrawalAmount = parseInt(document.getElementById("withdrawalAmount").value);
+    const currentBalance = parseInt(document.getElementById("initialDeposit").value);
+    //const currentBalance1 = parseInt(document.getElementById("currentBalance").placeholder);
+
+    let newBalance = currentBalance;
+    let newBalance1 = parseInt(newBalance);
+    console.log(currentBalance);
+    newBalance1 += depositAmount;
+    newBalance1 -= withdrawalAmount;
+    document.getElementById("currentBalance").innertext = toString(newBalance1);
+    console.log(newBalance1);
+
+}
+
+function handleNewAccount(event) {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const initialDeposit = document.getElementById("initialDeposit").value;
+    let newAccount = new Account(name, initialDeposit);
+    bankAccount.addAccount(newAccount);
+
+    const firstDeposit = document.getElementById("initialDeposit").value;
+    let currentBalance = document.getElementById("currentBalance");
+    currentBalance.placeholder = firstDeposit;
+    
+handleBalance();
+}
+
+
+window.addEventListener("load", function () {
+    const form = document.getElementById("form");
+    form.addEventListener("submit", handleNewAccount);
+});
